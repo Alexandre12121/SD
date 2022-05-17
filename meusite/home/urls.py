d843ccfdf9ba31feedbb969deacc9af04a65ed65
Alexandre Santos Ferreira <alex.santos.12121@gmail.com>
2022-05-17 16:02:42 -0300

    CSS
    Atualização CSS

--- meusite/home/urls.py
+++ meusite/home/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
 urlpatterns = [
     path('',views.index, name='index.html'),
     path('adicionar.html', views.adicionar, name='adicionar.html'),
-    path('index.html', views.artigos, name = 'artigos.html')
-]
+    path('index.html', views.artigos, name = 'artigos.html'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
