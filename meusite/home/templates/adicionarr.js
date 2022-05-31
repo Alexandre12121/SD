79931c901613de3f5167adb8eb46d651ad8b82ab
Vinícius Soares <vsspaulasouza@gmail.com>
2022-05-31 13:53:22 -0300

    feat: adicionando artigos

--- meusite/home/templates/adicionarr.js
+++ meusite/home/templates/adicionarr.js
@@ -1,32 +1,67 @@
-import Artigos from home.models
+const formulario = document.querySelector("#myForm");
+const titulo = document.querySelector("#titulo");
+const autores = document.querySelector("#autor");
+const instituicao = document.querySelector("#intituicao");
+const datapubli = document.querySelector("#datapubli");
+const datacria = document.querySelector("#datacria");
+const palachave = document.querySelector("#palavrachave");
+const resumo = document.querySelector("#resumo");
+const resume = document.querySelector("#resume");
+const referencias = document.querySelector("#ref");
+const direitos = document.querySelector("#direitos");
 
-const formulario = document.querySelector(".adicionar__form");
+const LINK_DE_ENVIO_DOS_ARTIGOS = "/artigos/";
 
-const LINK_DE_ENVIO_DOS_ARTIGOS = "";
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
 
 function handleSubmit(event) {
   event.preventDefault();
-  let dados = new FormData(formulario);  
-//       dados = {
-//       titulo: dados.get("titulo"),
-//       autor: dados.getAll("autor"),
-//       intituicao: dados.get("intituicao"),
-//       datapubli: dados.get("datapubli"),
-//       datacria: dados.get("datacria"),
-//       palavrachave: dados.getAll("palavrachave"),
-//       resumo: dados.get("resumo"),
-//       resume: dados.get("resume"),
-//       ref: dados.getAll("ref"),
-//       direitos: dados.get("direitos"),
-//     };
+  let dados = JSON.stringify({
+    titulo: titulo.value,
+    autores: autores.value,
+    instituicao: instituicao.value,
+    datapubli: datapubli.value,
+    datacria: datacria.value,
+    palachave: palachave.value,
+    resumo: resumo.value,
+    resume: resume.value,
+    referencias: referencias.value,
+    direitos: direitos.value,
+  });
   let options = {
     method: "POST",
     headers: {
-      //  "X-CSRFToken": getCookie("csrftoken"),
+      "X-CSRFToken": getCookie("csrftoken"),
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
     body: dados,
   };
   fetch(LINK_DE_ENVIO_DOS_ARTIGOS, options);
+
+  titulo.value = "";
+  autores.value = "";
+  instituicao.value = "";
+  datapubli.value = "";
+  datacria.value = "";
+  palachave.value = "";
+  resumo.value = "";
+  resume.value = "";
+  referencias.value = "";
+  direitos.value = "";
 }
 
 formulario.addEventListener("submit", handleSubmit);
